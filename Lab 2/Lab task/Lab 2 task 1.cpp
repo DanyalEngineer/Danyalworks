701819aa86854b668e431a936d92f8bf7726609c
Danyal Hassan <danyalhassan157@gmail.com>
2026-03-11 00:14:23 +0500

    Add files via upload

---
+++ Lab 2/Lab task/Lab 2 task 1.cpp
@@ -0,0 +1,21 @@
+//NAME: Danyal Hassan
+//ID: BF25NWELE0750
+//SEMESTER: 2nd
+//LAB: 2 (Programming)
+//DATE: 9/3/2026
+
+#include <iostream>
+  // Library used for input and output (cout, cin)
+using namespace std;
+// Allows us to use cout and cin without writing std::
+
+int main()
+// program start form hereinside these braces {} is the body of the program
+	{
+
+return 0;
+//program end here
+	}
+	
+	
+	
